ff52b6bf6abf20899cbc4d38ee5e6ea9d570520d
Peter Graham <peterghm@gmail.com>
2012-03-06 23:58:46 -0500

    refactor to make more readable

--- pixiv_quick_fullsize.user.js
+++ pixiv_quick_fullsize.user.js
@@ -8,6 +8,19 @@
 // ==/UserScript==
 
 (function() {
+  create_a = function(href, inner) {
+    var a = document.createElement("a");
+    a.href = href;
+    a.innerHTML = inner || "Direct Link &rarr;";
+    return a;
+  };
+  
+  create_img = function(src) {
+    var img = document.createElement("img");
+    img.src = src;
+    return img;
+  };
+  
   // replace medium images with full-size images
   var images = document.getElementsByTagName('img');
   var image_src_regex = new RegExp(/pixiv\.(net\/img\/[^\/]+\/[0-9]+)_m\./);
@@ -17,14 +30,9 @@
       var src = images[i].src.split(".");
       src[src.length - 2] = match[1];
       var full_size_url = src.join(".");
-      var direct_link = document.createElement("a");
-      direct_link.href = full_size_url;
-      direct_link.innerHTML = "Direct Link &rarr;";
       // have to create new element instead of just replacing img src
-      var new_img = document.createElement("img");
-      new_img.src = full_size_url;
-      images[i].parentNode.appendChild(direct_link);
-      images[i].parentNode.appendChild(new_img);
+      images[i].parentNode.appendChild(create_a(full_size_url));
+      images[i].parentNode.appendChild(create_img(full_size_url));
       images[i].parentNode.removeChild(images[i]);
     }
   }
