45f67558c50f29f681aeeedea1c966fd5a481eb2
Peter Graham <peterghm@gmail.com>
2012-03-07 18:03:35 -0500

    can't show fullsize of manga

--- pixiv_quick_fullsize.user.js
+++ pixiv_quick_fullsize.user.js
@@ -24,16 +24,17 @@
   // replace medium images with full-size images
   var images = document.getElementsByTagName('img');
   var image_src_regex = new RegExp(/pixiv\.(net\/img\/[^\/]+\/[0-9]+)_m\./);
+  var manga_url_regex = new RegExp(/member_illust\.php\?mode=manga/);
   for(var i=0; i< images.length; i++) {
     var match = image_src_regex.exec(images[i].src);
-    if(match) {
-      var src = images[i].src.split(".");
-      src[src.length - 2] = match[1];
-      var full_size_url = src.join(".");
-      // have to create new element instead of just replacing img src
-      images[i].parentNode.appendChild(create_a(full_size_url));
-      images[i].parentNode.appendChild(create_img(full_size_url));
-      images[i].parentNode.removeChild(images[i]);
-    }
+    if(!match) continue;
+    if(manga_url_regex.exec(images[i].parentNode.href)) continue;
+    var src = images[i].src.split(".");
+    src[src.length - 2] = match[1];
+    var full_size_url = src.join(".");
+    // have to create new element instead of just replacing img src
+    images[i].parentNode.appendChild(create_a(full_size_url));
+    images[i].parentNode.appendChild(create_img(full_size_url));
+    images[i].parentNode.removeChild(images[i]);
   }
 })();
